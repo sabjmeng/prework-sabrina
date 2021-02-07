4a586daaeece2c896dc4b8d459bf6c1e8b6e0aa9
Jun Han Jeff Xiang <jeffxiang@berkeley.edu>
2021-02-06 23:35:37 -0800

    Improve UI and design elements

--- prework-sabrina/ViewController.swift
+++ prework-sabrina/ViewController.swift
@@ -2,18 +2,40 @@
 //  ViewController.swift
 //  prework-sabrina
 //
-//  Created by Jeff Xiang on 2/6/21.
+//  Created by Sabrina Meng on 2/6/21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var billAmountTextField: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipAmountLabel.text = String(format:"$%.2f", tip)
+        
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
 }
 
